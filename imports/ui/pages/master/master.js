c7fc9b6fc323f47e84a7ce990cf45702b8df3881
marmar <andreasiskandar43@gmail.com>
2022-09-13 01:44:02 +0700

    some navbar top

--- imports/ui/pages/master/master.js
+++ imports/ui/pages/master/master.js
@@ -35,6 +35,8 @@ import '../../assets/master/js/jquery.cookie.js'
 import '../../assets/master/js/Chart.roundedBarCharts.js'
 // import './js/template.js'
 import '../../assets/master/js/dashboard.js' 
+import moment from 'moment';
+import Swal from 'sweetalert2';
 const navbar = [
   {
       type: 1,
@@ -331,6 +333,78 @@ function initPage() {
   // });
 }
 
+Template.navbarTop.onCreated(function () {
+  const self = this;
+  function greetings(){
+    var currentHour = +moment().format("HH");
+    // console.log(currentHour);
+    // https://stackoverflow.com/questions/62079752/using-moment-js-to-determine-if-current-time-in-hour-is-between-certain-hours
+    if (currentHour >= 3 && currentHour < 12){
+        return "Good Morning";
+    } else if (currentHour >= 12 && currentHour < 15){
+        return "Good Afternoon";
+    }   else if (currentHour >= 15 && currentHour < 20){
+        return "Good Evening";
+    } else if (currentHour >= 20 || currentHour < 3){
+        return "Good Night";
+    } else {
+        return "Hello"
+    }
+  
+  }
+  self.greetings = new ReactiveVar(greetings())
+  self.timeNow = new ReactiveVar(moment().locale('en').format('MMMM Do YYYY, h:mm:ss A'))
+  setInterval(() => {
+    self.greetings.set(greetings())
+  }, 3600000);
+  setInterval(() => {
+    if(screen.width < 990){
+      self.timeNow.set(moment().format("MMM Do YYYY"))
+    }
+    else{
+      self.timeNow.set(moment().locale('en').format('MMMM Do YYYY, h:mm:ss A'))
+    }
+  }, 500);
+})
+Template.navbarTop.helpers({
+  greetings(){
+    return Template.instance().greetings.get()
+  },
+  thisUser(){
+    const thisUser = Meteor.user()
+    // console.log(thisUser);
+    return thisUser
+  },
+  timeNow(){
+    return Template.instance().timeNow.get()
+  }
+});
+
+Template.navbarTop.events({
+  'click .sign-out'(e, t){
+    Swal.fire({
+      title: 'Do you want to logout now?',
+      icon: 'warning',
+      showDenyButton: true,
+      showCancelButton: false,
+      confirmButtonText: 'Yes',
+      denyButtonText: `No`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        // Swal.fire('Saved!', '', 'success')
+        Meteor.logout();
+        setTimeout(() => {
+          FlowRouter.go('/login')
+        }, 200);
+      } else if (result.isDenied) {
+        // Swal.fire('Changes are not saved', '', 'info')
+      }
+    })
+  }
+});
+
+
 Template.navbarLeft.onCreated(function () {
 })
 Template.navbarLeft.onRendered(function () {
@@ -365,6 +439,7 @@ Template.navbarLeft.helpers({
     return a == b
   }
 });
+
 Template.navbarLeft.events({
 });
 Template.masterContainer.onCreated(function () {
